a30a1b7393586c02a743a214d45256e0198475e3
ivlukin <pendulumfan40@gmail.com>
2017-08-24 14:46:44 +0300

    data augmenator

---
+++ CNN/data_augmentator.py
@@ -0,0 +1,42 @@
+import os
+
+import cv2
+
+
+def augmentate_set(directory, out, prefix):
+    """
+    augmentate all frames in directory
+    :param directory: directory which contains set
+    :param out: output directory
+    :param prefix: prefix for every augmented frame
+    :return: Void. Also, produces augmented frames to output directory
+    """
+    imgs = os.listdir(directory)
+    for img in imgs:
+        f = img[(img).rfind('.'):]
+        frame = cv2.imread(directory + '/' + img)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        augmentate_frame(frame, out, prefix, f)
+
+
+def augmentate_frame(frame, output, prefix, f):
+    """
+    augmentates frame
+    :param f: format of image to save
+    :param frame: frame to augmentate  
+    :param output: output directory
+    :param prefix: prefix to save
+    :return: 
+    """
+    from keras.preprocessing.image import ImageDataGenerator
+    f = frame.reshape((1,) + frame.shape)
+    datagen = ImageDataGenerator(rotation_range=10, horizontal_flip=True
+                                 )
+    i = 0
+    for batch in datagen.flow(f, batch_size=1, save_to_dir=output,
+                              save_format=f,
+                              save_prefix=prefix, ):
+        i += 1
+        if i > 2:
+            break
+
